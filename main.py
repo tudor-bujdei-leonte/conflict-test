a9f3a8b8eebf8e1946c5e5d1a4eae4c13cd016c4
Tudor Bujdei-Leonte <tudor.bujdei-leonte@student.manchester.ac.uk>
2021-10-24 13:14:23 +0100

    Updated master

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-print("Hello world! This is the non-conflicting incomplete final message.")
-
-print("Hello again! This is the post-conflict-resolution addition, still in the merge conflict!")
+print("This is the new master!")
